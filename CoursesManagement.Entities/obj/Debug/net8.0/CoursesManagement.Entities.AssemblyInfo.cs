3c6f11dcb42b0b31d12dd2c9fb8d037ad4ad8015
Marco Ehab <57108557+marcoehaab11@users.noreply.github.com>
2024-10-21 12:06:42 +0300

    .
    .

--- CoursesManagement.Entities/obj/Debug/net8.0/CoursesManagement.Entities.AssemblyInfo.cs
+++ CoursesManagement.Entities/obj/Debug/net8.0/CoursesManagement.Entities.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CoursesManagement.Entities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3a4c26b396d4b348a6d61140d3ab4c0744846c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b48dd09346bf55e976b8eb11b57114dde0c998bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("CoursesManagement.Entities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CoursesManagement.Entities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
